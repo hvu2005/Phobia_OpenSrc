49c2781ac46eaf987889a630440922d5a69672ce
hvu2005 <hoangvu241205@gmail.com>
2024-12-20 17:45:06 +0700

    ngon lanh

--- Assets/Script/MenuManager/MenuManager.cs
+++ Assets/Script/MenuManager/MenuManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject pauseOverlay;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject settingsMenu;
+
+    [Header("First Selected Options")]
+    [SerializeField] private GameObject resumeButton;
+
+
+
     private bool isPaused;
     // Start is called before the first frame update
     void Start()
@@ -46,23 +53,34 @@ public class MenuManager : MonoBehaviour
     {
         Time.timeScale = 1f;
         CloseAllMenus();
+        pauseOverlay.SetActive(false);
 
         InputManager.instance.canGetAction = true;
         settingsMenu.SetActive(false); 
     }
     public void OnOpenMainMenu()
     {
+
         mainMenu.SetActive(true);   
         settingsMenu.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(resumeButton);
     }
     public void OnOpenSettingsMenu()
     {
         mainMenu.SetActive(false);
         settingsMenu.SetActive(true);
     }
+
+    public void OnResume()
+    {
+        Unpause();
+    }
+
     private void CloseAllMenus()
     {
         mainMenu.SetActive(false);
         settingsMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
